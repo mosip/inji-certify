3edf328df576d576590c008bcbf249d044a4eca6
Challarao <challarao@beehyv.com>
2024-04-18 17:35:50 +0530

    bind volume for PKCS12 file in esignet Signed-off-by: Challarao <challarao@beehyv.com>

--- destroy.sh
+++ destroy.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 cd docker-compose-esignet
-docker compose down
+  docker compose down
+  sudo rm -rf data
 cd ..
 
 cd docker-compose-sunbird
 	docker compose down
-	sudo rm -rf data db-data
+	sudo rm -rf data
