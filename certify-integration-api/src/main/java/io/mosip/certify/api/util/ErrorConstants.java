5669ade3d0238bfe8302d3e9f3ea0e26d80fb350
Challarao <challarao@beehyv.com>
2024-06-18 11:20:25 +0530

    [INJICERT-189] revert package to io.mosip Signed-off-by: Challarao <challarao@beehyv.com>

---
+++ certify-integration-api/src/main/java/io/mosip/certify/api/util/ErrorConstants.java
@@ -0,0 +1,12 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+package io.mosip.certify.api.util;
+
+public class ErrorConstants {
+
+    public static final String NOT_IMPLEMENTED = "not_implemented";
+    public static final String VCI_EXCHANGE_FAILED = "vci_exchange_failed";
+}
