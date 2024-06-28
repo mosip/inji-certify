90b3dddfa5fb32fb1d690e3269329b1b1f47d8ee
Challarao <challarao@beehyv.com>
2024-06-28 11:46:36 +0530

    [INJICERT-37] rename certify database name Signed-off-by: Challarao <challarao@beehyv.com>

--- db_scripts/mosip_certify/dml.sql
+++ db_scripts/mosip_certify/dml.sql
@@ -3,7 +3,7 @@
 -- file, You can obtain one at https://mozilla.org/MPL/2.0/.
 -- -------------------------------------------------------------------------------------------------
 
-\c mosip_certify
+\c inji_certify
 
 
 \COPY certify.key_policy_def (APP_ID,KEY_VALIDITY_DURATION,PRE_EXPIRE_DAYS,ACCESS_ALLOWED,IS_ACTIVE,CR_BY,CR_DTIMES) FROM './dml/certify-key_policy_def.csv' delimiter ',' HEADER  csv;
