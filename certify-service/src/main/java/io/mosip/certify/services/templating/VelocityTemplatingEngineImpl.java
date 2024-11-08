71515881b65d6b223aad06fd45ac4702f756114c
Harsh Vardhan <harsh59v@gmail.com>
2024-11-08 18:33:45 +0530

    [INJICERT-434] compute svg template multibase&sha256 per spec (#125)
    * [INJICERT-434] compute svg template multibase&sha256 per spec
    
    ref: https://w3c-ccg.github.io/vc-render-method/#svgrenderingtemplate
    
    Signed-off-by: Harsh Vardhan <harsh59v@gmail.com>
    
    * [INJICERT-434] only fetch SVG template if VC 2.0 is requested
    
    Signed-off-by: Harsh Vardhan <harsh59v@gmail.com>
    
    ---------
    
    Signed-off-by: Harsh Vardhan <harsh59v@gmail.com>

--- certify-service/src/main/java/io/mosip/certify/services/templating/VelocityTemplatingEngineImpl.java
+++ certify-service/src/main/java/io/mosip/certify/services/templating/VelocityTemplatingEngineImpl.java
@@ -14,9 +14,13 @@ import java.util.*;
 import io.mosip.certify.api.spi.VCFormatter;
 import io.mosip.certify.core.constants.Constants;
 import io.mosip.certify.core.constants.VCDM2Constants;
+import io.mosip.certify.core.exception.TemplateException;
 import io.mosip.certify.core.repository.TemplateRepository;
+import io.mosip.certify.core.spi.SvgTemplateService;
+import io.mosip.certify.services.SVGRenderUtils;
 import jakarta.annotation.PostConstruct;
 import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.velocity.VelocityContext;
 import org.apache.velocity.app.VelocityEngine;
 import org.apache.velocity.runtime.RuntimeConstants;
@@ -28,6 +32,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import static io.mosip.certify.services.templating.VelocityTemplatingConstants.*;
+
+@Slf4j
 @Service
 public class VelocityTemplatingEngineImpl implements VCFormatter {
     VelocityEngine engine;
@@ -35,6 +42,8 @@ public class VelocityTemplatingEngineImpl implements VCFormatter {
     Map<String, String> templateCache;
     @Autowired
     TemplateRepository templateRepository;
+    @Autowired
+    SvgTemplateService svgTemplateService;
     @Value("${mosip.certify.vcformat.vc.expiry:true}")
     boolean shouldHaveDates;
 
@@ -66,8 +75,8 @@ public class VelocityTemplatingEngineImpl implements VCFormatter {
     @Override
     public String format(JSONObject templateInput, Map<String, Object> defaultSettings) {
         // TODO: Isn't template name becoming too complex with VC_CONTEXTS & CREDENTIAL_TYPES both?
-        String templateName = defaultSettings.get("templateName").toString();
-        String issuer = defaultSettings.get("issuerURI").toString();
+        String templateName = defaultSettings.get(TEMPLATE_NAME).toString();
+        String issuer = defaultSettings.get(ISSUER_URI).toString();
         String t = templateCache.get(templateName);
         StringWriter writer = new StringWriter();
         // 1. Prepare map
@@ -98,6 +107,15 @@ public class VelocityTemplatingEngineImpl implements VCFormatter {
         finalTemplate.put("_esc", new EscapeTool());
         // add the issuer value
         finalTemplate.put("issuer", issuer);
+        if (defaultSettings.containsKey(SVG_TEMPLATE) && templateName.contains(VCDM2Constants.URL)) {
+            try {
+                finalTemplate.put("_renderMethodSVGdigest",
+                        SVGRenderUtils.getDigestMultibase(svgTemplateService.getSvgTemplate(
+                                UUID.fromString((String) defaultSettings.get(SVG_TEMPLATE))).getTemplate()));
+            } catch (TemplateException e) {
+                log.error("SVG Template: " + defaultSettings.get(SVG_TEMPLATE) + " not available in DB", e);
+            }
+        }
         if (shouldHaveDates && !(templateInput.has(VCDM2Constants.VALID_FROM)
                 && templateInput.has(VCDM2Constants.VALID_UNITL))) {
             String time = ZonedDateTime.now(ZoneOffset.UTC).format(DateTimeFormatter.ofPattern(Constants.UTC_DATETIME_PATTERN));
