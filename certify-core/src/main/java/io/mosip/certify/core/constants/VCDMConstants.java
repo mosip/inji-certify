60dc0e86395b7d1b8b6e34790f85378716477fc0
Harsh Vardhan <harsh59v@gmail.com>
2024-11-19 08:59:58 +0530

    [INJICERT-434] add optional id field (#135)
    As per the published VC DM 1.1 spec, the ID field must be a URI but
    other first party implementations require ID field to be present for
    some other functions, hence this feature is being added temporarily.
    
    Signed-off-by: Harsh Vardhan <harsh59v@gmail.com>

--- certify-core/src/main/java/io/mosip/certify/core/constants/VCDMConstants.java
+++ certify-core/src/main/java/io/mosip/certify/core/constants/VCDMConstants.java
@@ -5,4 +5,5 @@ public class VCDMConstants {
     // the below assertionMethods are common b/w VC 1.1 & VC 2.0
     public static final String ASSERTION_METHOD = "assertionMethod";
     public static final String PROOF = "proof";
+    public static final String ID = "id";
 }
