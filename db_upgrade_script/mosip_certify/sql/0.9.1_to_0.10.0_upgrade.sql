30efe6c91798ee37dceb124084aef8200da9ccea
Piyush7034 <piyushshukla2100@gmail.com>
2024-12-09 15:41:19 +0530

    Fixed template name
    Signed-off-by: Piyush7034 <piyushshukla2100@gmail.com>

--- db_upgrade_script/mosip_certify/sql/0.9.1_to_0.10.0_upgrade.sql
+++ db_upgrade_script/mosip_certify/sql/0.9.1_to_0.10.0_upgrade.sql
@@ -26,8 +26,8 @@ CREATE TABLE IF NOT EXISTS template_data(
 
 COMMENT ON TABLE template_data IS 'Template Data: Contains velocity template for VC';
 
-COMMENT ON COLUMN svg_template.context IS 'VC Context: Context URL list items separated by comma(,)';
-COMMENT ON COLUMN svg_template.credential_type IS 'Credential Type: Credential type list items separated by comma(,)';
-COMMENT ON COLUMN svg_template.template IS 'Template Content: Velocity Template to generate the VC';
-COMMENT ON COLUMN svg_template.cr_dtimes IS 'Date when the template was inserted in table.';
-COMMENT ON COLUMN svg_template.upd_dtimes IS 'Date when the template was last updated in table.';
+COMMENT ON COLUMN template_data.context IS 'VC Context: Context URL list items separated by comma(,)';
+COMMENT ON COLUMN template_data.credential_type IS 'Credential Type: Credential type list items separated by comma(,)';
+COMMENT ON COLUMN template_data.template IS 'Template Content: Velocity Template to generate the VC';
+COMMENT ON COLUMN template_data.cr_dtimes IS 'Date when the template was inserted in table.';
+COMMENT ON COLUMN template_data.upd_dtimes IS 'Date when the template was last updated in table.';
