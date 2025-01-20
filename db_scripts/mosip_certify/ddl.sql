816fb408ec025b1c98795e857f154355224dd51c
Harsh Vardhan <harsh59v@gmail.com>
2025-01-20 19:09:11 +0530

    [INJICERT-657] add ca_cert_store table for keymanager
    Signed-off-by: Harsh Vardhan <harsh59v@gmail.com>

--- db_scripts/mosip_certify/ddl.sql
+++ db_scripts/mosip_certify/ddl.sql
@@ -3,6 +3,7 @@
 \ir ddl/certify-key_alias.sql
 \ir ddl/certify-key_policy_def.sql
 \ir ddl/certify-key_store.sql
+\ir ddl/certify-ca_cert_store.sql
 \ir ddl/certify-rendering_template.sql
 \ir ddl/certify-credential_template.sql
 
