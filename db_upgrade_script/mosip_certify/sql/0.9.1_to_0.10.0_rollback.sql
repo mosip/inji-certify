fcec9df3ea1e2d1e8d41cb5009285404288eb5cb
Harsh Vardhan <harsh59v@gmail.com>
2025-01-21 18:06:40 +0530

    [INJICERT-657] remove key_policy_defs from 0.9.1 version of Certify
    Signed-off-by: Harsh Vardhan <harsh59v@gmail.com>

--- db_upgrade_script/mosip_certify/sql/0.9.1_to_0.10.0_rollback.sql
+++ db_upgrade_script/mosip_certify/sql/0.9.1_to_0.10.0_rollback.sql
@@ -3,6 +3,4 @@ drop table credential_template;
 drop table ca_cert_store;
 
 --- Keymanager policy drop ---
-INSERT INTO certify.key_policy_def(APP_ID,KEY_VALIDITY_DURATION,PRE_EXPIRE_DAYS,ACCESS_ALLOWED,IS_ACTIVE,CR_BY,CR_DTIMES) VALUES('CERTIFY_VC_SIGN_RSA', 1095, 60, 'NA', true, 'mosipadmin', now());
-INSERT INTO certify.key_policy_def(APP_ID,KEY_VALIDITY_DURATION,PRE_EXPIRE_DAYS,ACCESS_ALLOWED,IS_ACTIVE,CR_BY,CR_DTIMES) VALUES('CERTIFY_VC_SIGN_ED25519', 1095, 60, 'NA', true, 'mosipadmin', now());
-INSERT INTO certify.key_policy_def(APP_ID,KEY_VALIDITY_DURATION,PRE_EXPIRE_DAYS,ACCESS_ALLOWED,IS_ACTIVE,CR_BY,CR_DTIMES) VALUES('BASE', 1095, 60, 'NA', true, 'mosipadmin', now());
+DELETE FROM certify.key_policy_def where APP_ID in ('CERTIFY_VC_SIGN_RSA', 'CERTIFY_VC_SIGN_ED25519', 'BASE');
