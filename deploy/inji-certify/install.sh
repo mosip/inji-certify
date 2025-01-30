066a0fe2e4170af1f6f5bccf8e7e66726e45f99b
Prafulrakhade <prafulrakhade@users.noreply.github.com>
2025-01-30 14:46:53 +0530

    [DSD-7171] inji certify 0.10.0 release
    Signed-off-by: techno-467 <prafulrakhade02@gmail.com>
    Signed-off-by: Harsh Vardhan <harsh59v@gmail.com>

--- deploy/inji-certify/install.sh
+++ deploy/inji-certify/install.sh
@@ -13,7 +13,7 @@ echo Create $SOFTHSM_NS namespace
 kubectl create ns $SOFTHSM_NS
 
 NS=inji-certify
-CHART_VERSION=0.10.0-develop
+CHART_VERSION=0.10.0
 
 echo Create $NS namespace
 kubectl create ns $NS
