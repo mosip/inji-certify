c99ff5fc6e32a77de4ebdbd5c9c57ee6a0fd9e70
Chandra Keshav Mishra <chandrakeshavmishra@gmail.com>
2025-02-03 19:52:25 +0530

    [DSD-7203] inji-certify 0.10.1 release (#208)
    * Updated Pom version and Chart version for post-release changes
    
    Signed-off-by: GitHub <noreply@github.com>
    
    * [DSD-7203] Update docker-compose.yaml
    
    Signed-off-by: Praful Rakhade <prafulrakhade02@gmail.com>
    
    * [DSD-7203] Updated values.yaml
    
    Signed-off-by: Praful Rakhade <prafulrakhade02@gmail.com>
    
    * [DSD-7203] Update install.sh
    
    Signed-off-by: Praful Rakhade <prafulrakhade02@gmail.com>
    
    * DSD-7203 inji certify 0.10.1 release
    
    Signed-off-by: techno-467 <prafulrakhade02@gmail.com>
    
    ---------
    
    Signed-off-by: GitHub <noreply@github.com>
    Signed-off-by: Praful Rakhade <prafulrakhade02@gmail.com>
    Signed-off-by: techno-467 <prafulrakhade02@gmail.com>
    Co-authored-by: Prafulrakhade <Prafulrakhade@users.noreply.github.com>
    Co-authored-by: Praful Rakhade <prafulrakhade02@gmail.com>

--- deploy/inji-certify/install.sh
+++ deploy/inji-certify/install.sh
@@ -13,7 +13,7 @@ echo Create $SOFTHSM_NS namespace
 kubectl create ns $SOFTHSM_NS
 
 NS=inji-certify
-CHART_VERSION=0.10.0-develop
+CHART_VERSION=0.10.1
 
 echo Create $NS namespace
 kubectl create ns $NS
