401a8805227de1fea9e554abcff329aa32f7f01b
Chandra Keshav Mishra <chandrakeshavmishra@gmail.com>
2025-02-21 14:33:44 +0530

    [DSD-7433] inji certify 0.10.2 post release changes (#245)
    * Updated Pom version and Chart version for post-release changes
    
    Signed-off-by: GitHub <noreply@github.com>
    
    * Updated install.sh
    
    Signed-off-by: Praful Rakhade <prafulrakhade02@gmail.com>
    
    ---------
    
    Signed-off-by: GitHub <noreply@github.com>
    Signed-off-by: Praful Rakhade <prafulrakhade02@gmail.com>
    Co-authored-by: Prafulrakhade <Prafulrakhade@users.noreply.github.com>
    Co-authored-by: Praful Rakhade <prafulrakhade02@gmail.com>

--- deploy/inji-certify/install.sh
+++ deploy/inji-certify/install.sh
@@ -13,7 +13,7 @@ echo Create $SOFTHSM_NS namespace
 kubectl create ns $SOFTHSM_NS
 
 NS=inji-certify
-CHART_VERSION=0.10.2
+CHART_VERSION=0.10.2-develop
 
 echo Create $NS namespace
 kubectl create ns $NS
