bbb447dae950b88d441118867f5c96e984d3017a
amaydixit11 <amaydixit11@gmail.com>
2025-04-03 12:21:24 +0530

    Fixed Reviewed Changes

--- docker-compose/docker-compose-injistack/certify_init.sql
+++ docker-compose/docker-compose-injistack/certify_init.sql
@@ -152,7 +152,6 @@ INSERT INTO certify.credential_template (context, credential_type, template, cr_
     "validFrom": "${validFrom}",
     "validUntil": "${validUntil}",
     "credentialSubject": {
-        "id": "${_holderId}",
         "fullName": "${fullName}",
         "mobileNumber": "${mobileNumber}",
         "dateOfBirth": "${dateOfBirth}",
