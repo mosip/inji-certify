b1f8fad60c66e9ee7d10e38804d1a0c71008a7b0
Harsh Vardhan <harsh59v@gmail.com>
2025-05-28 11:50:47 +0530

    [INJICERT-1032] fix error message for invalid credential request
    Signed-off-by: Harsh Vardhan <harsh59v@gmail.com>

--- certify-core/src/main/java/io/mosip/certify/core/validators/CredentialRequestValidatorFactory.java
+++ certify-core/src/main/java/io/mosip/certify/core/validators/CredentialRequestValidatorFactory.java
@@ -1,7 +1,9 @@
 package io.mosip.certify.core.validators;
 
+import io.mosip.certify.core.constants.ErrorConstants;
 import io.mosip.certify.core.constants.VCFormats;
 import io.mosip.certify.core.dto.CredentialRequest;
+import io.mosip.certify.core.exception.InvalidRequestException;
 import lombok.extern.slf4j.Slf4j;
 
 //todo replace usage of existing validators with this factory
@@ -17,7 +19,7 @@ public class CredentialRequestValidatorFactory {
                 return new SDJWTVcCredentialRequestValidator().isValidCheck(credentialRequest);
             default:
                 log.debug("Unsupported or Invalid request format {} ", credentialRequest.getFormat());
-                return false;
+                throw new InvalidRequestException(ErrorConstants.UNSUPPORTED_VC_FORMAT);
         }
         
     }
