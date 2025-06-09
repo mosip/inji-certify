d5b1f75832fe5f7d88d0aba5886058958c54e95b
SradhaMohanty5899 <mohantysradha10@gmail.com>
2025-06-09 15:59:27 +0530

    MOSIP-28246 Removed commented unused variables
    Signed-off-by: SradhaMohanty5899 <mohantysradha10@gmail.com>

--- api-test/src/main/java/io/mosip/testrig/apirig/injicertify/utils/InjiCertifyUtil.java
+++ api-test/src/main/java/io/mosip/testrig/apirig/injicertify/utils/InjiCertifyUtil.java
@@ -1067,7 +1067,6 @@ public class InjiCertifyUtil extends AdminTestUtil {
 						GlobalConstants.MOSIP_ESIGNET_ID_TOKEN_EXPIRE_SECONDS));
 		JWSSigner signer;
 		String proofJWT = "";
-//		String typ = "openid4vci-proof+jwt";
 		SignedJWT signedJWT = null;
 
 		try {
