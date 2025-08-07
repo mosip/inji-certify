23ed50547c097aac05abe72ce749768ae096a5eb
Anmol Mangaraj <mangarajanmol666@gmail.com>
2025-08-07 22:25:45 +0530

    Add OAuth AS metadata endpoint and configuration
    - Added OAuthASMetadataDTO for OAuth 2.0 Authorization Server metadata
    - Implemented OAuthASMetadataService to handle metadata generation
    - Added /.well-known/oauth-authorization-server endpoint in WellKnownController
    - Updated application-local.properties with OAuth AS configuration properties
    
    Signed-off-by: Anmol Mangaraj <mangarajanmol666@gmail.com>
    Signed-off-by: anmol5936 <127973342+anmol5936@users.noreply.github.com>

--- certify-service/src/main/java/io/mosip/certify/controller/WellKnownController.java
+++ certify-service/src/main/java/io/mosip/certify/controller/WellKnownController.java
@@ -1,8 +1,10 @@
 package io.mosip.certify.controller;
 
 import io.mosip.certify.core.dto.CredentialIssuerMetadataDTO;
+import io.mosip.certify.core.dto.OAuthASMetadataDTO;
 import io.mosip.certify.core.spi.CredentialConfigurationService;
 import io.mosip.certify.core.spi.VCIssuanceService;
+import io.mosip.certify.services.OAuthASMetadataService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,6 +23,9 @@ public class WellKnownController {
     @Autowired
     private VCIssuanceService vcIssuanceService;
 
+    @Autowired
+    private OAuthASMetadataService oAuthASMetadataService;
+
     @GetMapping(value = "/openid-credential-issuer", produces = "application/json")
     public CredentialIssuerMetadataDTO getCredentialIssuerMetadata(
             @RequestParam(name = "version", required = false, defaultValue = "latest") String version) {
@@ -30,6 +35,11 @@ public class WellKnownController {
     @GetMapping(value = "/did.json")
     public Map<String, Object> getDIDDocument() {
         return vcIssuanceService.getDIDDocument();
+    }
+
+    @GetMapping(value = "/oauth-authorization-server", produces = "application/json")
+    public OAuthASMetadataDTO getOAuthASMetadata() {
+        return oAuthASMetadataService.getOAuthASMetadata();
     }
 }
 
