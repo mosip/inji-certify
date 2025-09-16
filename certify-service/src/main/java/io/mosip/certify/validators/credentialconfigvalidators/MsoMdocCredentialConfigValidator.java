80db69623cce8f91fa79e13d4864955f165fb271
jackjain <jackjain1995@gmail.com>
2025-09-16 16:45:06 +0530

    [INJICERT-1193] Refactor credential configuration service to convert to entity only after validation passes
    Signed-off-by: jackjain <jackjain1995@gmail.com>

--- certify-service/src/main/java/io/mosip/certify/validators/credentialconfigvalidators/MsoMdocCredentialConfigValidator.java
+++ certify-service/src/main/java/io/mosip/certify/validators/credentialconfigvalidators/MsoMdocCredentialConfigValidator.java
@@ -1,20 +1,21 @@
 package io.mosip.certify.validators.credentialconfigvalidators;
 
+import io.mosip.certify.core.dto.CredentialConfigurationDTO;
 import io.mosip.certify.entity.CredentialConfig;
 import io.mosip.certify.repository.CredentialConfigRepository;
 
 import java.util.Optional;
 
 public class MsoMdocCredentialConfigValidator {
-    public static boolean isValidCheck(CredentialConfig credentialConfig) {
+    public static boolean isValidCheck(CredentialConfigurationDTO credentialConfig) {
         return credentialConfig.getDocType() != null && !credentialConfig.getDocType().isEmpty()
                 && credentialConfig.getSignatureCryptoSuite() != null && !credentialConfig.getSignatureCryptoSuite().isEmpty()
-                && credentialConfig.getCredentialType() == null && credentialConfig.getContext() == null
-                && credentialConfig.getSdJwtVct() == null && credentialConfig.getCredentialSubject() == null
+                && (credentialConfig.getCredentialTypes() == null || credentialConfig.getCredentialTypes().isEmpty()) && (credentialConfig.getContextURLs() == null || credentialConfig.getContextURLs().isEmpty())
+                && credentialConfig.getSdJwtVct() == null && credentialConfig.getCredentialSubjectDefinition() == null
                 && credentialConfig.getSdJwtClaims() == null;
     }
 
-    public static boolean isConfigAlreadyPresent(CredentialConfig credentialConfig,
+    public static boolean isConfigAlreadyPresent(CredentialConfigurationDTO credentialConfig,
                                                  CredentialConfigRepository credentialConfigRepository) {
         Optional<CredentialConfig> optional =
                 credentialConfigRepository.findByCredentialFormatAndDocType(
