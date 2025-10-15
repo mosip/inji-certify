c929b57a87fa9261c5cce37b9c397291a3b9649c
Amay Dixit <65495380+amaydixit11@users.noreply.github.com>
2025-10-15 11:27:32 +0530

    Altered MsoMdocCredentialRequestValidator to fit the new change that claims are not used in the request anymore
    Signed-off-by: amaydixit11 <amaydixit11@gmail.com>

--- certify-service/src/main/java/io/mosip/certify/validators/MsoMdocCredentialRequestValidator.java
+++ certify-service/src/main/java/io/mosip/certify/validators/MsoMdocCredentialRequestValidator.java
@@ -4,9 +4,6 @@ import io.mosip.certify.core.dto.CredentialRequest;
 
 public class MsoMdocCredentialRequestValidator {
     public static boolean isValidCheck(CredentialRequest credentialRequest) {
-        if (credentialRequest.getDoctype() == null || credentialRequest.getDoctype().isBlank()) {
-            return false;
-        }
-        return credentialRequest.getClaims() != null && !credentialRequest.getClaims().isEmpty();
+        return credentialRequest.getDoctype() != null && !credentialRequest.getDoctype().isBlank();
     }
 }
