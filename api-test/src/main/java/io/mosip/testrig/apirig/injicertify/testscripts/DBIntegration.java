9af68657e76520a45deee435b10654cfe35761b1
likhitharl <91238496+likhitharl@users.noreply.github.com>
2025-11-14 14:09:59 +0530

    Mosip-43621  - Automated CSR endpoints (Generate , Upload CA , Upload signed certificate) (#498)
    * Mosip-43621
    
    Signed-off-by: Likhitha R L <likhitha.rl@technoforte.co.in>
    
    * mosip-43621
    
    Signed-off-by: Likhitha R L <likhitha.rl@technoforte.co.in>
    
    * mosip-43621
    
    Signed-off-by: Likhitha R L <likhitha.rl@technoforte.co.in>
    
    * mosip-43621
    
    Signed-off-by: Likhitha R L <likhitha.rl@technoforte.co.in>
    
    ---------
    
    Signed-off-by: Likhitha R L <likhitha.rl@technoforte.co.in>
    Co-authored-by: Likhitha R L <likhitha.rl@technoforte.co.in>

--- api-test/src/main/java/io/mosip/testrig/apirig/injicertify/testscripts/DBIntegration.java
+++ api-test/src/main/java/io/mosip/testrig/apirig/injicertify/testscripts/DBIntegration.java
@@ -84,7 +84,7 @@ public class DBIntegration extends InjiCertifyUtil implements ITest {
 
 		String inputJson = getJsonFromTemplate(testCaseDTO.getInput(), testCaseDTO.getInputTemplate());
 
-		inputJson = InjiCertifyUtil.inputStringKeyWordHandeler(inputJson, testCaseName);
+		inputJson = inputStringKeyWordHandeler(inputJson, testCaseName);
 
 		JSONObject jsonObject = new JSONObject(inputJson);
 		String sqlQuery = jsonObject.getString("db_query");
